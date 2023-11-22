2dc31971656603de80dcf24fbb9beb0cb7c77af9
ozingozing <hoon4367@gmail.com>
2023-11-22 12:22:28 +0900

    [FIX] from 7 to 8

--- PyOpenGL/07_CameraNprojectoin copy.py
+++ PyOpenGL/07_CameraNprojectoin copy.py
@@ -3,41 +3,28 @@ from OpenGL.GLU import *
 
 from PyQt6.QtWidgets import *
 from PyQt6.QtOpenGLWidgets import *
+
 import sys
-def DrawAxes() :
-    glBegin(GL_LINES)
-    glColor(1, 0, 0)
-    glVertex3f(0, 0, 0)
-    glVertex3f(1, 0 ,0)
 
-    glColor(0, 1, 0)
-    glVertex3f(0, 0, 0)
-    glVertex3f(0, 1 ,0)
 
-    glColor(0, 0, 1)
-    glVertex3f(0, 0, 0)
-    glVertex3f(0, 0 ,1)
-    glEnd()
-
-class MyGLWindow(QOpenGLWidget) : #QOpenGLWidget 상속
+class MyGLWindow(QOpenGLWidget) :
     def __init__(self) :
-        super().__init__()  #QMainWindow 생성자 실행
-        self.setWindowTitle("glOrtho 연습!")
+        super().__init__()
+        self.setWindowTitle("glOrtho")
 
     def initializeGL(self) :
         glClearColor(0.1, 0.7, 0.3, 1.0)
 
     def resizeGL(self, w: int, h: int) :
-        aspR = w/h
-        range = 1
+        aspRatio = w / h
+        range = 2
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
-        
-        glOrtho(-range*aspR, range*aspR, 
-                -range, range, -range, range)
-    
-    def paintGL(slef) :
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glOrtho(-range * aspRatio, range * aspRatio, -range , range, -range, range)
+
+
+    def paintGL(self) :
+        glClear(GL_COLOR_BUFFER_BIT |  GL_DEPTH_BUFFER_BIT)
 
         glBegin(GL_POLYGON)
         glColor3f(1, 1, 0)
@@ -46,12 +33,12 @@ class MyGLWindow(QOpenGLWidget) : #QOpenGLWidget 상속
         glVertex3f(-1, 0, 0)
         glVertex3f( 0, -1, 0)
         glEnd()
-        DrawAxes()
 
 def main(argv = sys.argv) :
     app = QApplication(argv)
     window = MyGLWindow()
     window.show()
+
     app.exec()
 
 if __name__ == "__main__" :
